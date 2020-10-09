5b76beb14472bfc0614ede3ed3cdf8e7ad273a3d
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-10-09 09:50:47 +0200

    Add project files.

---
+++ redKeyDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redKeyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key please...");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine(key.Key.ToString());
+
+            if (key.Key.ToString() == "enter")
+            {
+                Console.WriteLine("ENTER");
+            }
+            if (key.Key.ToString() == "enter")
+            {
+                Console.WriteLine("ENTER");
+            }
+
+            Console.WriteLine("you pressed Enter key");
+
+            Console.ReadKey();
+        }
+    }
+}
